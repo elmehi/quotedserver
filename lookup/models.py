10315ff0726733ade073a000e8bc52798b062262
Elliot Hirsch <ehirsch@princeton.edu>
2016-04-04 03:35:55 -0400

    back on heroku code hosting

---
+++ lookup/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# A source model.
+class Source(models.Model):
+    quote = models.TextField()
+    url = models.CharField(max_length = 200)
+    title = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 200)
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.quote
---
+++ lookup/views.py
@@ -0,0 +1,86 @@
+# import requests
+# from django.shortcuts import render
+from django.http import HttpResponse
+from googleapiclient.discovery import build
+import pprint
+import json
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# from .models import Source
+
+# returns all sources in database
+# def db(request):
+#     sources = Source()
+#     sources.save()
+#     sources = Source.objects.all()
+
+#     return render(request, 'db.html', {'sources': sources})
+
+# JS guys need a get request
+# def getSource(request):
+#     newSource = Source()
+#     newSource.name = "test"
+#     jsonData = {
+#         name:
+#     }
+#     newSource.save()
+#     HttpResponse(json.dumps(jsonData), content_type='application/json')
+
+#logic to check and see if in databse - would do a get all objects then iterate and check
+#if this returns null - can't find anything - go to google, add to database
+
+# def lookup(request):
+
+#     url = request.url
+#     parameters = request.parameters
+#     text = parameters.text
+
+    
+
+#     #create source out of google response and add to cache
+#     results = goToGoolgeTop(text)
+
+#     newSource = Source()
+#     newSource.url = results["url"]
+#     newSource.title = results["title"]
+#     newSource.author = results["author"]
+#     newSource.name = results["name"]
+#     newSource.date = results["date"]
+
+#     newSource.save()
+
+
+    # get all sources
+
+    # iterate through sources
+
+    # if matching source found, returns
+
+    # else, go to google and look up source for text
+    #      create new Source() object and save() to database
+
+    # return source
+
+
+# def goToGoogleFirst(text):
+
+
+
+def goToGoogleTop(text):
+    service = build("customsearch", "v1", developerKey="AIzaSyABOiui8c_-sFGJSSXCk6tbBThZT2NI4Pc")
+    res = service.cse().list(q = text, cx='006173695502366383915:cqpxathvhhm',).execute()
+    # pprint.pprint(res)
+    # print type(res)
+    first = res["items"][0]
+    # print first
+    if first["pagemap"]["metatags"][0]: meta = first["pagemap"]["metatags"][0]
+    pageinfo = {
+      'quote':text, 'title': first["title"], 'url': first["link"]
+      # 'name': first["pagemap"]["metatags"][0]["og:site_name"]
+    }
+    if "og:site_name" in meta.keys():
+      pageinfo["name"] = meta["og:site_name"]
+    return pageinfo
