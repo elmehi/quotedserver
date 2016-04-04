7d022201bcbef98a4ff8c3936dc59fc8662755a3
Elliot Hirsch <ehirsch@princeton.edu>
2016-04-04 01:00:41 -0400

    stuff

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
 
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
-
 
 def db(request):
 
@@ -16,4 +14,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
