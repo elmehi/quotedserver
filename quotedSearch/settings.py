c3e6f40e1b7ba3cf80031d7b1f54ce05b2457097
jkvlevin <jake.k.levin@gmail.com>
2016-04-14 14:24:57 -0400

    caching implemented

--- quotedSearch/settings.py
+++ quotedSearch/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lookup'
+    'lookup.apps.LookupConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
