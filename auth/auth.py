258a6d364d3b0063b8b19c0883113a79ffa1fc2a
Eric Mitchell <eam6@princeton.edu>
2016-04-19 13:28:55 -0400

    Added server test output.

--- auth/auth.py
+++ auth/auth.py
@@ -1,2 +1,5 @@
 def authenticate(request):
+    print request.META
+    print request.META['Authorization']
     
+    return HttpResponse('pageinfo', content_type='application/text')
--- signup/signup.py
+++ signup/signup.py
@@ -4,4 +4,5 @@ from ..lookup.models import User
 def sign_up_user(request):
     print request.META
     print request.META['Authorization']
-    if User.objects.filter(username)
+    
+    return HttpResponse('pageinfo', content_type='application/text')
