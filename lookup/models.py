ae1bfca208c74c9bcabe9f5a983e9ca46c6e4af2
jkvlevin <jake.k.levin@gmail.com>
2016-04-19 19:25:52 -0400

    added auth

--- lookup/models.py
+++ lookup/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+import django
 from django.db import models
 import datetime
 
@@ -9,7 +9,7 @@ class Source(models.Model):
     source_url = models.CharField(max_length = 200)
     source_title = models.CharField(max_length = 200)
     source_name = models.CharField(max_length = 200)
-    source_date = models.DateTimeField(default=datetime.date.today())
+    source_date = models.DateTimeField(default=django.utils.timezone.now)
 
     def __str__(self):
         return self.source_quote
@@ -18,12 +18,12 @@ class User(models.Model):
 	username = models.CharField(max_length = 200)
 	password = models.CharField(max_length = 20)
 	highlight_url = models.BooleanField(default = True)
-	date_created = models.DateTimeField(default=datetime.date.today())
+	date_created = models.DateTimeField(default=django.utils.timezone.now)
 
 	def __str__(self):
 		return self.username
 
 class Request(models.Model):
 	user_id = models.BigIntegerField()
-	request_date = models.DateTimeField(default=datetime.date.today())
+	request_date = models.DateTimeField(default=django.utils.timezone.now)
 	request_source = models.ForeignKey('Source', on_delete=models.DO_NOTHING)
