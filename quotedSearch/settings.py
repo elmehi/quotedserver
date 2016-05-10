25e97bcbba209a90e61877afd654a64374d912ac
Eric Mitchell <eam6@princeton.edu>
2016-05-10 15:38:05 -0400

    Removed extensions.

--- quotedSearch/settings.py
+++ quotedSearch/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lookup.apps.LookupConfig',
-    'django_extensions',
     # 'django.contrib.sites',
     # 'django.contrib.flatpages',
     # 'tests.apps.TestsConfig',
